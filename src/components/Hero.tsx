d1eff89ca3c0e1b5411f1284c633f404f2414172
Bahadır Can <bahadircan.bc@gmail.com>
2024-02-25 13:47:11 +0300

    added sparkles and bento grid

---
+++ src/components/Hero.tsx
@@ -0,0 +1,39 @@
+import VideoThumb from "@/public/images/hero-image-01.jpg";
+import { SparklesCore } from "./Sparkles";
+
+export default function Hero() {
+  return (
+    <section>
+      <div className="w-[72rem] mx-auto px-4 sm:px-6 relative">
+        {/* Hero content */}
+        <div className="pt-32 pb-10 md:pt-40 md:pb-16">
+          {/* Section header */}
+          <div className="w-[48rem] mx-auto text-center relative">
+            <h1 className="text-8xl mb-4 font-poiret-one" data-aos="fade-up">
+              dream
+            </h1>
+            <div className="w-[48rem] h-40 relative">
+
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-yellow-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-yellow-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-px w-1/4" />
+
+              {/* Core component */}
+              <SparklesCore
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#FFFFFF"
+              />
+              <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(500px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,97 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gray: {
+          100: '#EBF1F5',
+          200: '#D9E3EA',
+          300: '#C5D2DC',
+          400: '#9BA9B4',
+          500: '#707D86',
+          600: '#55595F',
+          700: '#33363A',
+          800: '#25282C',
+          900: '#151719',
+        },
+        purple: {
+          100: '#F4F4FF',
+          200: '#E2E1FF',
+          300: '#CBCCFF',
+          400: '#ABABFF',
+          500: '#8D8DFF',
+          600: '#5D5DFF',
+          700: '#4B4ACF',
+          800: '#38379C',
+          900: '#262668',
+        },
+        jmpurple: {
+          100: '#F3E8FF',
+          200: '#E3C9FF',
+          300: '#DAAFFF',
+          400: '#C17AFF',
+          500: '#9D33D6',
+          600: '#7A2AA3',
+          700: '#5C1E7A',
+          800: '#3D1251',
+          900: '#20082D',
+        },
+        jmorange: {
+          100: '#FFF3E8',
+          200: '#FFE3C9',
+          300: '#FFDAAF',
+          400: '#FFC17A',
+          500: '#E36732',
+          600: '#C14F2A',
+          700: '#A13D23',
+          800: '#7D2E19',
+          900: '#5A1F10',
+        },
+      },
+      spacing: {
+        '9/16': '56.25%',
+        '3/4': '75%',
+        '1/1': '100%',
+      },
+      fontFamily: {
+        inter: ['var(--font-inter)', 'sans-serif'],
+        'poiret-one': ['var(--font-poiret-one)', 'sans-serif']
+      },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+        '4xl': '2.5rem',
+        '5xl': '3.25rem',
+        '6xl': '4rem',
+      },
+      inset: {
+        'full': '100%',
+      },
+      letterSpacing: {
+        tighter: '-0.02em',
+        tight: '-0.01em',
+        normal: '0',
+        wide: '0.01em',
+        wider: '0.02em',
+        widest: '0.4em',
+      },
+      minWidth: {
+        '10': '2.5rem',
+      },
+      scale: {
+        '98': '.98'
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
